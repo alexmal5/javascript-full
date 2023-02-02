e10b995abf492a6208e27699c17124ac3070e8f9
alexmal5 <113168776+alexmal5@users.noreply.github.com>
2023-02-02 17:03:29 +0200

    Add files via upload

---
+++ lesson_13_modules_and_unit_tests/task_4_testing_calculator/calculator.test.js
@@ -0,0 +1,26 @@
+import { calc } from './calculator.js';
+
+it('should get 4', () => {
+  const result = calc('2 + 2');
+  expect(result).toEqual('4');
+});
+
+it('should get 0', () => {
+  const result = calc('2 - 2');
+  expect(result).toEqual('0');
+});
+
+it('should get 4', () => {
+  const result = calc('2 * 2');
+  expect(result).toEqual('4');
+});
+
+it('should get 1', () => {
+  const result = calc('2 / 2');
+  expect(result).toEqual('1');
+});
+
+it('should get null', () => {
+  const result = calc('2 / 2');
+  expect(result).toEqual('1');
+});
