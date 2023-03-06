175762a92a2fe7312706f4ff1b55710b05b1acc6
alexmal5 <113168776+alexmal5@users.noreply.github.com>
2023-03-06 15:12:36 +0200

    Add files via upload

---
+++ lesson_21_DOM/task_2_finding_element_on_page/index.js
@@ -0,0 +1,15 @@
+const getTitleElement = () => {
+  const titleElement = document.querySelector('.title');
+  console.dir(titleElement);
+  return titleElement;
+};
+
+getTitleElement();
+
+const getInputElement = () => {
+  const inputElement = document.querySelector('[type="text"]]');
+  console.dir(inputElement);
+  return inputElement;
+};
+
+getInputElement();
