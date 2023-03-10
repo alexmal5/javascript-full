f66091989f4e78e2e44f138f8f754a48a30f1216
alexmal5 <113168776+alexmal5@users.noreply.github.com>
2023-03-10 09:15:43 +0200

    Add files via upload

--- lesson_22_events_browser_handling_bubbling/task_1_event_bubbling_and_sinking/index.js
+++ lesson_22_events_browser_handling_bubbling/task_1_event_bubbling_and_sinking/index.js
@@ -12,21 +12,21 @@ const logGreenP = logTarget.bind(null, 'P', 'green');
 const logGreenSpan = logTarget.bind(null, 'SPAN', 'green');
 
 const logGreyDiv = logTarget.bind(null, 'DIV', 'grey');
-const logGreynP = logTarget.bind(null, 'P', 'grey');
+const logGreyP = logTarget.bind(null, 'P', 'grey');
 const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
 
-const click = () => {
+const addClick = () => {
   divElem.addEventListener('click', logGreyDiv, true);
   divElem.addEventListener('click', logGreenDiv);
 
-  pElem.addEventListener('click', logGreyDiv, true);
-  pElem.addEventListener('click', logGreenDiv);
+  pElem.addEventListener('click', logGreyP, true);
+  pElem.addEventListener('click', logGreenP);
 
-  spanElem.addEventListener('click', logGreyDiv, true);
-  spanElem.addEventListener('click', logGreenDiv);
+  spanElem.addEventListener('click', logGreySpan, true);
+  spanElem.addEventListener('click', logGreenSpan);
 };
 
-click();
+addClick();
 
 const clear = () => {
   const eventsListElem = document.querySelector('.events-list');
@@ -37,17 +37,17 @@ const buttonClear = document.querySelector('.clear-btn');
 buttonClear.addEventListener('click', clear);
 
 const buttonAttach = document.querySelector('.attach-handlers-btn');
-buttonAttach.addEventListener('click', addHandleClick);
+buttonAttach.addEventListener('click', addClick);
 
 const removeClick = () => {
   divElem.removeEventListener('click', logGreyDiv, true);
   divElem.removeEventListener('click', logGreenDiv);
 
-  pElem.removeEventListener('click', logGreyDiv, true);
-  pElem.removeEventListener('click', logGreenDiv);
+  pElem.removeEventListener('click', logGreyP, true);
+  pElem.removeEventListener('click', logGreenP);
 
-  spanElem.removeEventListener('click', logGreyDiv, true);
-  spanElem.removeEventListener('click', logGreenDiv);
+  spanElem.removeEventListener('click', logGreySpan, true);
+  spanElem.removeEventListener('click', logGreenSpan);
 };
 
 const buttonRemove = document.querySelector('.remove-handlers-btn');
