9339ea9017c373a0c36ce7de81e883e29baef860
alexmal5 <113168776+alexmal5@users.noreply.github.com>
2023-03-29 12:15:21 +0300

    Add files via upload

--- lesson_23_events_forms_delegation_part_1/task_3_todo_list_create_update_task/index.js
+++ lesson_23_events_forms_delegation_part_1/task_3_todo_list_create_update_task/index.js
@@ -1,28 +1,31 @@
 const tasks = [
-  { text: 'Buy milk', done: false },
-  { text: 'Pick up Tom from airport', done: false },
-  { text: 'Visit party', done: false },
-  { text: 'Visit doctor', done: true },
-  { text: 'Buy meat', done: true },
+  { text: 'Buy milk', done: false, id: '1' },
+  { text: 'Pick up Tom from airport', done: false, id: '2' },
+  { text: 'Visit party', done: false, id: '3' },
+  { text: 'Visit doctor', done: true, id: '4' },
+  { text: 'Buy meat', done: true, id: '5' },
 ];
 
 const listElem = document.querySelector('.list');
 
 const renderTasks = (tasksList) => {
+  const listItemElems = document.querySelectorAll('.list__item');
+  listItemElems.forEach((el) => el.remove());
+
   const tasksElems = tasksList
     .sort((a, b) => a.done - b.done)
-    .map(({ text, done }) => {
+    .map(({ text, done, id }) => {
       const listItemElem = document.createElement('li');
       listItemElem.classList.add('list__item');
       const checkbox = document.createElement('input');
       checkbox.setAttribute('type', 'checkbox');
       checkbox.checked = done;
       checkbox.classList.add('list__item-checkbox');
+      checkbox.dataset.id = id;
       if (done) {
         listItemElem.classList.add('list__item_done');
       }
       listItemElem.append(checkbox, text);
-
       return listItemElem;
     });
 
