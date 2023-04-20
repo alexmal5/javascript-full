7af508d002b5824b5b4c13f3f40ad2aca6f6a4c0
alexmal5 <113168776+alexmal5@users.noreply.github.com>
2023-04-20 15:40:53 +0300

    Add files via upload

---
+++ lesson_24_working_with_dates/task_1_installing_date_components/index.js
@@ -0,0 +1,8 @@
+const nameOfDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+
+export const dayOfWeek = (date, days) => {
+  const startDay = new Date(date).getDate();
+  const newDay = new Date(date).setDate(startDay + days);
+
+  return nameOfDays[new Date(newDay).getDay()];
+};
